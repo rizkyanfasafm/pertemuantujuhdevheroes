edc2eaf5f529b07d363296fcf97f5c1e88b0d4a3
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2019-12-15 18:02:41 +0700

    update baru

--- pokemon.dart
+++ pokemon.dart
@@ -1,6 +1,6 @@
 main(){
-  var pokemon = Pokedex("Pokemon", 200);
-  var bulbasaur = Pokedex("Bulbasaur", 300);
+  var pokemon = Pokedex("Pokemon", 200,10);
+  var bulbasaur = Pokedex("Bulbasaur", 300,15);
   var tmpr = Tempur();
 
   pokemon.cetak();
@@ -12,8 +12,9 @@ main(){
 class Pokedex{
   String nama;
   int kekuatan;
+  int damage;
 
-  Pokedex(this.nama,this.kekuatan);
+  Pokedex(this.nama,this.kekuatan,this.damage);
 
   void cetak(){
     print("Nama ${this.nama} Kekuatan ${this.kekuatan}");
@@ -22,10 +23,12 @@ class Pokedex{
 
 class Tempur{
   void attack(Pokedex poke1, Pokedex poke2){
-    if(poke1.kekuatan >= poke2.kekuatan){
-      print("${poke1.nama} menang dengan kekuatan ${poke1.kekuatan}");
-    }else{
-      print("${poke2.nama} menang dengan kekuatan ${poke2.kekuatan}");
-    }
+    // if(poke1.kekuatan >= poke2.kekuatan){
+    //   print("${poke1.nama} menang dengan kekuatan ${poke1.kekuatan}");
+    // }else{
+    //   print("${poke2.nama} menang dengan kekuatan ${poke2.kekuatan}");
+    // }
+    poke2.kekuatan -= poke1.damage;
+    print("si ${poke1.nama} menyerang dengan damage ${poke1.damage} menyebabkan ${poke2.kekuatan} ${poke2.nama} berkurang");
   }
 }
